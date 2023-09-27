cdca5a9772c27f8062d3437a3a7d44899506300a
BehroozMansouri <b.m.mansouri@gmail.com>
2023-09-26 22:38:20 -0400

    update IStack and Implement myStack

---
+++ src/xmlvalidator/myStack.java
@@ -0,0 +1,37 @@
+package xmlvalidator;
+
+import java.util.LinkedList;
+
+public class myStack implements IStack{
+    LinkedList<String> stack;
+    
+    public myStack()
+    {
+        stack = new LinkedList<String>();
+    }
+    
+    @Override
+    public void push(String s) {
+        // TODO Auto-generated method stub
+        stack.addFirst(s);
+    }
+
+    @Override
+    public String pop() {
+        // TODO Auto-generated method stub
+        return stack.removeFirst();
+    }
+
+    @Override
+    public String peek() {
+        // TODO Auto-generated method stub
+        return stack.getFirst();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return stack.isEmpty();
+    }
+
+}
